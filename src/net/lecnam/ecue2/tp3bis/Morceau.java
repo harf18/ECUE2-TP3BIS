8987eb4686f2e5c6fa496e85f337a4ccfec6507e
Maxime MONICHON--SIRONNEAU <maxime.monichonsironneau@gmail.com>
2026-09-21 14:06:11 +0200

    Exercice 3

--- src/net/lecnam/ecue2/tp3bis/Morceau.java
+++ src/net/lecnam/ecue2/tp3bis/Morceau.java
@@ -6,4 +6,20 @@ public class Morceau {
     int nbEcoute;
     Duree tps;
 
+    public Morceau(String title, String artist, Duree d){
+        this.titre = title;
+        this.artiste = artist;
+        this.tps = d;
+        this.nbEcoute = 0;
+    }
+
+    public Morceau(String title, String artist, int minutes, int secondes){
+        this.titre = title;
+        this.artiste = artist;
+        this.tps = new Duree(minutes, secondes);
+        this.nbEcoute = 0;
+    }
+
+
+
 }
